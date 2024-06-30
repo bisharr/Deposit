aa0d072c47a140b311866aad8fc6d59271f7da84
bisharr <bishar995@gmail.com>
2024-06-30 20:07:15 +0300

    login Validate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.onload = function () {
   document.getElementById('swithToReg').onclick = switchToReg;
 
   document.getElementById('swithToLogIn').onclick = switchToLogIN;
+  document.getElementById('login-btn').onclick = loginValidtion;
 
   function switchToReg() {
     document.getElementById('register-portal').style.display = 'inline-block';
@@ -10,5 +11,22 @@ window.onload = function () {
   function switchToLogIN() {
     document.getElementById('register-portal').style.display = 'none';
     document.getElementById('login-portal').style.display = 'inline-block';
+  }
+
+  //..acc no pin pattern...
+
+  var AccNoPat = '^[0-9]{6}$';
+  var accPinPat = '^[0-9]{4}$';
+
+  //Login validation...
+  function loginValidtion() {
+    var LAccNo = document.getElementById('lAccNo').value;
+    var LPinNo = document.getElementById('lAccPin').value;
+
+    if (LAccNo.match(AccNoPat) && LPinNo.match(accPinPat)) {
+      alert('Welcome');
+    } else {
+      alert('Please enter Valid details');
+    }
   }
 };
