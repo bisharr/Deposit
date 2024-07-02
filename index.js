427c4a1dd29233c6bfb05177528f104e0b963140
bisharr <bishar995@gmail.com>
2024-07-03 00:43:35 +0300

    fire

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+//imports and config firebase
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  child,
+  update,
+  remove,
+} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDmYWSGucyB9GU44hXYXkkco8Um2La9vdw',
+  authDomain: 'abc-bank-c1fab.firebaseapp.com',
+  projectId: 'abc-bank-c1fab',
+  storageBucket: 'abc-bank-c1fab.appspot.com',
+  messagingSenderId: '9706879880',
+  appId: '1:9706879880:web:21164ac0c06c780c60583d',
+};
+//WINDOW*************//
 window.onload = function () {
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase();
+  const dbref = ref(db);
+
   document.getElementById('swithToReg').onclick = switchToReg;
 
   document.getElementById('swithToLogIn').onclick = switchToLogIN;
@@ -15,6 +41,7 @@ window.onload = function () {
   }
 
   //..acc no pin pattern...
+  console.log('helo');
 
   var AccNoPat = '^[0-9]{6}$';
   var accPinPat = '^[0-9]{4}$';
@@ -26,6 +53,7 @@ window.onload = function () {
 
     if (LAccNo.match(AccNoPat) && LPinNo.match(accPinPat)) {
       alert('Welcome to your account');
+      portal(LAccNo, LPinNo);
     } else {
       alert('Please enter Valid details');
     }
@@ -45,9 +73,34 @@ window.onload = function () {
       rAccPin.match(accPinPat) &&
       rAccPin == rConAccPin
     ) {
-      alert('account registered successfully');
+      set(ref(db, 'accNo ' + rAccNo + '/accPin' + rAccPin + '/accDetails'), {
+        name: rAccName,
+        avalBal: 0,
+      })
+        .then(() => {
+          alert('account registered successfully');
+        })
+        .catch((error) => {
+          alert('Registered Failed\n' + error);
+        });
+      set(ref(db, 'accNo ' + rAccNo + '/received'), {
+        receivedAmt: 0,
+      })
+        .then(() => {
+          console.log('received amt updated');
+        })
+        .catch((error) => {
+          alert('received updation Failed\n' + error);
+        });
     } else {
       alert('Please enter Valid details');
     }
+  }
+
+  //Protal**********
+  function portal(accNo, accPin) {
+    document.getElementById('register-portal').style.display = 'none';
+    document.getElementById('login-portal').style.display = 'none';
+    document.getElementById('portal').style.display = 'inline-block';
   }
 };
